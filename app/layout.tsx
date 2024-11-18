82071a5cc89cd3bcf966c87b96e2c7beb4f8d51b
Moritz Wright <moritzwright@MacBookPro.fritz.box>
2024-11-18 20:04:26 +0100

    why is this not workiiiiiiiing

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,38 +6,38 @@ import "./globals.css";
 const sofiaProSoft = localFont({
   src: [
     {
-      path: "/fonts/SofiaProSoftLight.woff",
+      path: "fonts/SofiaProSoftLight.woff",
       weight: "300",
     },
     {
-      path: "/fonts/SofiaProSoftLight-italic.woff",
+      path: "fonts/SofiaProSoftLight-italic.woff",
       weight: "300",
       style: "italic",
     },
     {
-      path: "/fonts/SofiaProSoftRegular.woff",
+      path: "fonts/SofiaProSoftRegular.woff",
       weight: "400",
     },
     {
-      path: "/fonts/SofiaProSoftRegular-Italic.woff",
+      path: "fonts/SofiaProSoftRegular-Italic.woff",
       weight: "400",
       style: "italic",
     },
     {
-      path: "/fonts/SofiaProSoftMedium.woff",
+      path: "fonts/SofiaProSoftMedium.woff",
       weight: "500",
     },
     {
-      path: "/fonts/SofiaProSoftMedium-italic.woff",
+      path: "fonts/SofiaProSoftMedium-italic.woff",
       weight: "500",
       style: "italic",
     },
     {
-      path: "/fonts/SofiaProSoftBold.woff",
+      path: "fonts/SofiaProSoftBold.woff",
       weight: "700",
     },
     {
-      path: "/fonts/SofiaProSoftBold-italic.woff",
+      path: "fonts/SofiaProSoftBold-italic.woff",
       weight: "700",
       style: "italic",
     },
