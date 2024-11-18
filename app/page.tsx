bc74738aad7dc34e59d5afdd8208fbb53fe8e075
Moritz Wright <moritzwright@MacBookPro.fritz.box>
2024-11-18 19:12:43 +0100

    now should be fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex flex-col items-center justify-center relative font-primary">
+    <main className="h-screen w-screen flex flex-col items-center justify-center relative font-primary bg-secondary">
       <Image src="/logo.svg" alt="Logo" width={300} height={300} />
       <div className="grid gap-4">
         <h1 className="font-medium text-primary text-5xl text-center">
